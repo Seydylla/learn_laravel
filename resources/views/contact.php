f7a13f602e000a8a4f6c1dbddf64a4db8463ff04
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-08-07 23:29:24 +0500

    1.0.3

--- resources/views/contact.php
+++ resources/views/contact.php
@@ -2,9 +2,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About page</title>
+    <title>Contact page</title>
 </head>
 <body>
-    <h1>Hello from about page</h1>
+    <h1>Hello from contact page</h1>
 </body>
 </html>
--- resources/views/home.php
+++ resources/views/home.php
@@ -2,9 +2,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About page</title>
+    <title>Home page</title>
 </head>
 <body>
-    <h1>Hello from about page</h1>
+    <h1>Hello from home page</h1>
 </body>
 </html>
