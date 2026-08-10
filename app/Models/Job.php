9486e7741dbb42b7f7d3f7e7b60fb4ef910ce748
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-08-10 21:10:16 +0500

    1.0.13

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Support\Arr;
 
 class Job{
     public static function all(): array {
@@ -24,5 +25,15 @@ class Job{
             ]
         ];
 
+    }
+
+    public static function find(int $id): array {
+        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
+
+        if(! $job) {
+            abort(404);
+        }
+
+        return $job;
     }
 }
