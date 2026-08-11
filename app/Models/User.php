68c8fb7cecd25d77136aee2fd259f4644c7e3f35
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-08-11 22:51:25 +0500

    1.0.14

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,13 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
     protected function casts(): array
     {
         return [
