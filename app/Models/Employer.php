1d376f8e74d58f0b7b1ca07f84dab25fcd959a89
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-08-12 23:13:06 +0500

    1.0.18

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+//  Model: It is for connection between database and laravel
+
 class Employer extends Model
 {
     /** @use HasFactory<\Database\Factories\EmployerFactory> */
