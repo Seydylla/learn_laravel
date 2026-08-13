e8a2e2f1a1d859c98125e13bf1f951b64ea437ce
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-08-13 23:39:24 +0500

    1.0.30

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -7,7 +7,12 @@
     <div class="space-y-4">
         @foreach ($jobs as $job)
             <a href="/jobs/{{$job['id']}}" class="block px-4 py-6 border light:border-gray-200 dark:border-gray-800 rounded-lg">
-                <li><strong>{{$job['title']}}</strong>: Pays {{$job['salary']}} per year</li>
+
+                <div class="font-bold text-blue-500 test-sm">{{ $job->employer->name }}</div>
+
+                <div>
+                    <strong>{{$job['title']}}</strong>: Pays {{$job['salary']}} per year
+                </div>
             </a>
         @endforeach
     </div>
