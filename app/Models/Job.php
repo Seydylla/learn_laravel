542bb772fe0f28911836b9e498e70a9f96a73a43
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-08-13 22:41:21 +0500

    1.0.27

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -16,6 +16,6 @@ class Job extends Model{
     }
 
     public function tags() {
-        return $this->belongsToMany(Tag::class);
+        return $this->belongsToMany(Tag::class, foreignPivotKey:'job_listing_id');
     }
 }
