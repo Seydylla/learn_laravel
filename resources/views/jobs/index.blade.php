946c7a9342a67b0b9a391c18d89657cb47f83feb
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-08-14 22:49:37 +0500

    1.0.41

---
+++ resources/views/jobs/index.blade.php
@@ -0,0 +1,23 @@
+<x-layout>
+
+    <x-slot:heading>
+        Jobs page
+    </x-slot:heading>
+
+    <div class="space-y-4">
+        @foreach ($jobs as $job)
+            <a href="/jobs/{{$job['id']}}" class="block px-4 py-6 border light:border-gray-200 dark:border-gray-800 rounded-lg">
+
+                <div class="font-bold text-blue-500 test-sm">{{ $job->employer->name }}</div>
+
+                <div>
+                    <strong>{{$job['title']}}</strong>: Pays {{$job['salary']}} per year
+                </div>
+            </a>
+        @endforeach
+
+        <div>
+            {{$jobs->links()}}
+        </div>
+    </div>
+</x-layout>
