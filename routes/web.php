9fbb733bf561a14c1b37b9eaabe3055b386413a2
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-08-14 14:07:03 +0500

    1.0.38

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,8 @@ Route::get('/jobs', function (){
 
     //  This reduces query times and just use one time query
 
-    $job = Job::with('employer')->paginate(3);
+    $job = Job::with('employer')->simplePaginate(3);
+    // $job = Job::with('employer')->cursorPaginate(3); //  This is datas which we don't need to show the number of page link
     return view('jobs', [
         'jobs' => $job
     ]);
