0fdc23af7ee55b3a82cc68d6fa5d9e381322f48a
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-08-18 19:57:46 +0500

    1.0.56

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,19 @@
 <?php
 
-use App\Models\Job;
-
-use App\Http\Controllers;
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Arr;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
-Route::get('/jobs', [JobController::class, 'index']);
-
-Route::get('/jobs/create', [JobController::class, 'create']);
-
-Route::post('/jobs', [JobController::class, 'store']);
-
-Route::get('/jobs/{job}', [JobController::class, 'show']);
-
-Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-
-Route::patch('/jobs/{job}', [JobController::class, 'update']);
-
-Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
-
-//  If put here(under of the parent conn, it will be give an error. We will have to put above of that)
-// Route::get('/jobs/create', function () {
-//     return view('...');
+// Route::controller(JobController::class)->group(function() {
+//     Route::get('/jobs', 'index');
+//     Route::get('/jobs/create', 'create');
+//     Route::post('/jobs', 'store');
+//     Route::get('/jobs/{job}', 'show');
+//     Route::get('/jobs/{job}/edit', 'edit');
+//     Route::patch('/jobs/{job}', 'update');
+//     Route::delete('/jobs/{job}', 'destroy');
 // });
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::resource('jobs', JobController::class);
