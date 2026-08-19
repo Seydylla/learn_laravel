3930bfa761c0650e95df039e6ac58c154210a44f
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-08-19 23:07:27 +0500

    1.0.59

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -15,7 +15,7 @@
                     <div class="sm:col-span-4">
                         <x-form-label for="title">Title</x-form-label>
                             <div class="mt-2">
-                                <x-form-input id="title" name="title"/>
+                                <x-form-input id="title" name="title" placeholder="Project manager"/>
 
                                 <x-form-error name="title" />
                             </div>
@@ -24,9 +24,7 @@
                     <div class="sm:col-span-4">
                         <label for="salary" class="block text-sm/6 font-medium text-gray-900 dark:text-white">Salary</label>
                         <div class="mt-2">
-                            <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600 dark:bg-white/5 dark:outline-white/10 dark:focus-within:outline-indigo-500">
-                            <input id="salary" type="text" name="salary" placeholder="$50,000 Per Year" required class="block min-w-0 grow bg-white py-1.5 px-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6 dark:bg-transparent dark:text-white dark:placeholder:text-gray-500" />
-                            </div>
+                            <x-form-input id="salary" name="salary" placeholder="$50,000 USD"/>
 
                             <x-form-error name="salary" />
                         </div>
