e36caed36fc650426ff6fd118375678be3fa3f51
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-08-19 22:54:32 +0500

    1.0.57

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -13,15 +13,13 @@
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                     <div class="sm:col-span-4">
-                        <label for="title" class="block text-sm/6 font-medium text-gray-900 dark:text-white">title</label>
+                        <x-form-label for="title">Title</x-form-label>
                             <div class="mt-2">
                                 <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600 dark:bg-white/5 dark:outline-white/10 dark:focus-within:outline-indigo-500">
                                 <input id="title" type="text" name="title" placeholder="Shift Leader" required class="block min-w-0 grow bg-white py-1.5 px-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6 dark:bg-transparent dark:text-white dark:placeholder:text-gray-500" />
                                 </div>
 
-                                @error('title')
-                                    <p class="test-xs text-red-500 font-semibold"> {{$message}} </p>
-                                @enderror
+                                <x-form-error name="title"></x-form-error>
                             </div>
                         </div>
 
@@ -32,9 +30,7 @@
                             <input id="salary" type="text" name="salary" placeholder="$50,000 Per Year" required class="block min-w-0 grow bg-white py-1.5 px-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6 dark:bg-transparent dark:text-white dark:placeholder:text-gray-500" />
                             </div>
 
-                            @error('salary')
-                                <p class="test-xs text-red-500 font-semibold"> {{$message}} </p>
-                            @enderror
+                            <x-form-error name="salary"></x-form-error>
                         </div>
                     </div>
                 </div>
