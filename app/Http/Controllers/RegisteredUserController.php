7c06cbee9dada08bdb6d97ec757d2f78b57f6740
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-08-20 23:25:32 +0500

    1.0.67

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class RegisteredUserController extends Controller
 {
@@ -11,6 +14,21 @@ class RegisteredUserController extends Controller
     }
 
     public function store() {
-        dd('Good evening');
+
+    //  validate
+        $sttributes = request()->validate([
+            'fist_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', Password::min(6), 'confirmed'],
+        ]);
+
+        //  Create the user
+
+        $user = User::create($sttributes);
+
+        //Log in
+
+        Auth::login($user);
     }
 }
